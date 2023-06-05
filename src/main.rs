a084e2006b67856b403183a6d1bd93a921305f1e
Rouan van der Ende <rouan@8bo.org>
2023-06-05 15:18:11 +0200

    smoltcp failing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![deny(unsafe_code)]
-#![deny(warnings)]
+// #![deny(warnings)]
 #![no_main]
 #![no_std]
 
@@ -16,8 +16,8 @@ use crate::hal::{pac, prelude::*};
 use rtt_target::{rprintln, rtt_init_print};
 
 // use embedded_nal::TcpClientStack;
-
 use embedded_nal::TcpClientStack;
+use smoltcp_nal;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -34,9 +34,9 @@ fn main() -> ! {
     let gpiod = p.GPIOD.split();
 
     let mut led_green = gpiod.pd12.into_push_pull_output();
-    let mut led_orange = gpiod.pd13.into_push_pull_output();
-    let mut led_red = gpiod.pd14.into_push_pull_output();
-    let mut led_blue = gpiod.pd15.into_push_pull_output();
+    // let mut led_orange = gpiod.pd13.into_push_pull_output();
+    // let mut led_red = gpiod.pd14.into_push_pull_output();
+    // let mut led_blue = gpiod.pd15.into_push_pull_output();
 
     // let mut cs: OutputPin = p.GPIOA.split().pa3.into_push_pull_output(); // chip select
 
@@ -85,71 +85,113 @@ fn main() -> ! {
 
     let cs = gpioa.pa3.into_push_pull_output();
 
-    let mut device = w5500::UninitializedDevice::new(FourWire::new(spi2, cs))
-        .initialize_manual(
-            MacAddress::new(0, 1, 2, 3, 4, 5),
-            embedded_nal::Ipv4Addr::new(192, 168, 50, 87),
-            w5500::Mode::default(),
-        )
-        .unwrap();
+    // let mut device = w5500::UninitializedDevice::new(FourWire::new(spi2, cs))
+    //     .initialize_manual(
+    //         MacAddress::new(0, 1, 2, 3, 4, 5),
+    //         embedded_nal::Ipv4Addr::new(192, 168, 50, 87),
+    //         w5500::Mode::default(),
+    //     )
+    //     .unwrap();
 
-    let mut counter: usize = 0;
+    // .initialize_macraw(MacAddress::new(0, 1, 2, 3, 4, 5)).unwrap();
+    //     // .initialize(MacAddress::new(0, 1, 2, 3, 4, 5), w5500::Mode::default())
+    //     // .unwrap();
+
+    //     device.write_frame(frame)
 
     let version = device.version().unwrap();
+    rprintln!("Version {:?}", version);
+
+    let mut counter: usize = 0;
+    // let mut socket = device.socket().unwrap();
+
+    // device
+    //     .connect(
+    //         &mut socket,
+    //         embedded_nal::SocketAddr::new(
+    //             embedded_nal::IpAddr::V4(embedded_nal::Ipv4Addr::new(192, 168, 50, 86)),
+    //             1337,
+    //         ),
+    //     )
+    //     .unwrap();
+
+    let config = smoltcp_nal::smoltcp::iface::Config::new();
+
+    let mut iface = smoltcp_nal::smoltcp::iface::Interface::new(config, &mut device);
+
+    let mut dhcp_socket = smoltcp_nal::smoltcp::socket::dhcpv4::Socket::new();
+    dhcp_socket.set_max_lease_duration(Some(smoltcp_nal::smoltcp::time::Duration::from_secs(10)));
+
+    let mut sockets = smoltcp_nal::smoltcp::iface::SocketSet::new([]);
+    let dhcp_handle = sockets.add(dhcp_socket);
 
     loop {
-        let mut socket = device.socket().unwrap();
-
-        device
-            .connect(
-                &mut socket,
-                embedded_nal::SocketAddr::new(
-                    embedded_nal::IpAddr::V4(embedded_nal::Ipv4Addr::new(192, 168, 50, 86)),
-                    1337,
-                ),
-            )
-            .unwrap();
-
-        device
-            .send(&mut socket, &[104, 101, 108, 108, 111, 10])
-            .unwrap();
-
-        device.close(socket).unwrap();
-
-        rprintln!("Version {:?}", version);
-        for _ in 0..10_000 {
-            led_green.set_high();
-        }
-        for _ in 0..10_000 {
-            led_green.set_low();
-        }
-
-        for _ in 0..10_000 {
-            led_orange.set_high();
-        }
-        for _ in 0..10_000 {
-            led_orange.set_low();
-        }
-
-        for _ in 0..10_000 {
-            led_red.set_high();
-        }
-        for _ in 0..10_000 {
-            led_red.set_low();
-        }
-
-        for _ in 0..10_000 {
-            led_blue.set_high();
-        }
-        for _ in 0..10_000 {
-            led_blue.set_low();
-        }
+        let timestamp = smoltcp_nal::smoltcp::time::Instant::now();
+        iface.poll(timestamp, &mut device, &mut sockets);
 
         counter += 1;
+        rprintln!("Sending {}", counter);
 
-        if counter > 10 {
-            panic!("This is an intentional panic.");
-        }
+        // device
+        //     .send(&mut socket, &[104, 101, 108, 108, 111, 10])
+        //     .unwrap();
+
+        let event = sockets
+            .get_mut::<smoltcp_nal::smoltcp::socket::dhcpv4::Socket>(dhcp_handle)
+            .poll();
+
+        // device.send(&mut socket, &[counter as u8]).unwrap();
+
+        // let mut recvbuf = [0u8; 1024];
+        // // let mut asdf: &str = "aaaaaaaaaaaaaa";
+        // loop {
+        //     let result = device.receive(&mut socket, &mut recvbuf).unwrap();
+        //     // asdf = core::str::from_utf8(&recvbuf).unwrap();
+
+        //     if result > 0 {
+        //         rprintln!("Data recv {:?} {} {}", result, recvbuf[0], recvbuf[1]);
+        //         // rprintln!("Data recv {:?} text:{:?}", result, recvbuf);
+        //         // rprintln!("Data recv {} {:?}", result, recvbuf);
+        //         break;
+        //     }
+        // }
+
+        // }
+        // device.close(socket).unwrap();
+
+        // for _ in 0..10_000 {
+        //     led_green.set_high();
+        // }
+        // for _ in 0..10_000 {
+        //     led_green.set_low();
+        // }
+
+        // for _ in 0..10_000 {
+        //     led_orange.set_high();
+        // }
+        // for _ in 0..10_000 {
+        //     led_orange.set_low();
+        // }
+
+        // for _ in 0..10_000 {
+        //     led_red.set_high();
+        // }
+        // for _ in 0..10_000 {
+        //     led_red.set_low();
+        // }
+
+        // for _ in 0..10_000 {
+        //     led_blue.set_high();
+        // }
+        // for _ in 0..10_000 {
+        //     led_blue.set_low();
+        // }
+
+        // counter += 1;
+
+        // if counter > 10 {
+        //     panic!("This is an intentional panic.");
+        // }
     }
 }
 
